50c0eb9208ea9bce14d10e734d6acf72e51d056f
haroldpop <harold.popluhar@orange.fr>
2026-02-08 18:41:08 +0100

    Merge branch 'feature/pop_up_intervention_' into feature/refacto_authentication

--- scripts/core-windows/fresh_start_db.ps1
+++ scripts/core-windows/fresh_start_db.ps1
@@ -4,5 +4,4 @@ Write-Host "Kill possible docker containers residuals"
 docker ps -a --filter "name=supabase_.*_CRM_template" -q | ForEach-Object { docker rm -f $_ }
 Write-Host "Restarting supabase"
 supabase start
-supabase functions serve --no-verify-jwt
 pause
